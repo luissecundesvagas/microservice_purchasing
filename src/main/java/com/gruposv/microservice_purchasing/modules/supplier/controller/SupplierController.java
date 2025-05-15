faa99661492aca13e90c170143192d510f078366
luissecundes <luissecundes@gmail.com>
2025-05-15 19:09:52 -0300

    add controller

--- src/main/java/com/gruposv/microservice_purchasing/modules/supplier/controller/SupplierController.java
+++ src/main/java/com/gruposv/microservice_purchasing/modules/supplier/controller/SupplierController.java
@@ -1,5 +1,26 @@
 package com.gruposv.microservice_purchasing.modules.supplier.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.gruposv.microservice_purchasing.modules.supplier.entity.SupplierEntity;
+import com.gruposv.microservice_purchasing.modules.supplier.service.SupplierService;
+
+@RestController
+@RequestMapping("/api/suppliers")
 public class SupplierController {
-    
+
+    private final SupplierService supplierService;
+
+    @Autowired
+    public SupplierController(SupplierService supplierService) {
+        this.supplierService = supplierService;
+    }
+
+    @PostMapping
+    public ResponseEntity<SupplierEntity> createSupplier(@RequestBody SupplierEntity supplier) {
+        SupplierEntity savedSupplier = supplierService.saveSupplier(supplier);
+        return ResponseEntity.ok(savedSupplier);
+    }
 }

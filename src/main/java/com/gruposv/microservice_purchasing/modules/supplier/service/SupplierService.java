65f1538851c8e7fafd23f2c56ca2b839d8639bf5
luissecundes <luissecundes@gmail.com>
2025-05-15 19:08:52 -0300

    implementação do service

--- src/main/java/com/gruposv/microservice_purchasing/modules/supplier/service/SupplierService.java
+++ src/main/java/com/gruposv/microservice_purchasing/modules/supplier/service/SupplierService.java
@@ -1,5 +1,22 @@
 package com.gruposv.microservice_purchasing.modules.supplier.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.gruposv.microservice_purchasing.modules.supplier.entity.SupplierEntity;
+import com.gruposv.microservice_purchasing.modules.supplier.repository.SupplierRepository;
+
+@Service
 public class SupplierService {
-    
+
+    private final SupplierRepository supplierRepository;
+
+    @Autowired
+    public SupplierService(SupplierRepository supplierRepository) {
+        this.supplierRepository = supplierRepository;
+    }
+
+    public SupplierEntity saveSupplier(SupplierEntity supplier) {
+        return supplierRepository.save(supplier);
+    }
 }
